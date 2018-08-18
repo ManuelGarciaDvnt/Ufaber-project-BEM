ac4560bfc37aea14d33d904c92ad78d1b3a914d6
manueldev135 <manueldev250799@gmail.com>
2018-08-17 20:10:46 -0500

    estructurando los links para el slider

--- src/js/main_home.js
+++ src/js/main_home.js
@@ -1,7 +1,19 @@
 $(function(){
-    $(".s-cover__next-section").click(function () {
-        $(".s-header").css('display', 'none');
-    });    
+    if($(window).scrollTop()>100){
+        $('#s-header__menu').addClass('s-header--sticky');
+    }else{
+        $('#s-header__menu').removeClass('s-header--sticky');
+    }
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 100) {
+            $('#s-header__menu').addClass('s-header--sticky');
+        }
+        else
+            $('#s-header__menu').removeClass('s-header--sticky');
+    });
+
+    
+
 });
 
 
