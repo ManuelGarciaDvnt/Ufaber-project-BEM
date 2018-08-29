3196feca80f5c345e54f0658bdcbb949dded5a38
manueldev135 <manueldev250799@gmail.com>
2018-08-28 19:38:10 -0500

    Avance del 28 de agosto

--- src/js/s-courses.js
+++ src/js/s-courses.js
@@ -1,15 +1,22 @@
 (function(){
 var hearts= $(".s-courses__heart");
+var covers = $(".s-courses__cover");
 hearts.on('click', function(){
     var isActive= $(this).attr('class');
     if(isActive==="s-courses__heart s-courses__heart--active"){
     console.log("ya esta favorito");
     $(this).removeClass("s-courses__heart--active");
+    $(this).parent().removeClass("s-courses__cover--favorite");
     }
-    else
-    $(this).addClass("s-courses__heart--active");
-    console.log("Aun no estaba activo");
+
+    else if (isActive !== "s-courses__heart s-courses__heart--active"){
+        $(this).addClass("s-courses__heart--active");
+        $(this).parent().addClass("s-courses__cover--favorite");
+        console.log("Aun no estaba activo");
+    }
     
+
+
     ;
     
 });
